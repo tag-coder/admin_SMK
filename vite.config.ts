f84d615ec22a214177f014f530445934fb1d8ddb
tag-coder <tag.malang12@gmail.com>
2026-05-06 01:05:14 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // 👈 Tambahkan baris ini agar path index.html aman
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
